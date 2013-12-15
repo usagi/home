6cb4c146f64392e8fd9163e824ba1cee7e5ab260
Usagi Ito <usagi@WonderRabbitProject.net>
2013-12-15 15:47:31 +0900

    fix bug: job table full or recursion limit exceeded

--- .zshrc.d/.zsh
+++ .zshrc.d/.zsh
@@ -26,22 +26,22 @@ autoload predict-on
 #zle -N predict-on
 #zle -N predict-off
 
-alias predict-on="predict-on && export ZSH_PREDICT_STATE=0"
-alias predict-off="predict-off && export ZSH_PREDICT_STATE=1"
+alias predict.on="predict-on && export ZSH_PREDICT_STATE=0"
+alias predict.off="predict-off && export ZSH_PREDICT_STATE=1"
 
-predict-on
+predict.on
 
-predict-toggle()
+predict.toggle()
 {
   if [ $ZSH_PREDICT_STATE -eq 0 ]
-  then predict-off;
-  else predict-on;
+  then predict.off;
+  else predict.on;
   fi;
 }
 
-zle -N predict-toggle
+zle -N predict.toggle
 
-bindkey "^[[2~" predict-toggle
+bindkey "^[[2~" predict.toggle
 
 zstyle ':predict' verbose true
 
