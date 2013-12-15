8f607c50589015875d4d4007ae3ab1afd03e1f7f
Usagi Ito <usagi@WonderRabbitProject.net>
2013-12-15 12:28:16 +0900

    add predict-toggle

--- .zshrc.d/.zsh
+++ .zshrc.d/.zsh
@@ -15,13 +15,34 @@ bindkey "^[[3~" delete-char
 
 setopt auto_cd
 setopt auto_pushd
-setopt correct
+#setopt correct
 setopt list_packed
-#setopt nolistbeep
-setopt noautoremoveslash
+setopt nolistbeep
+#setopt noautoremoveslash
 setopt complete_aliases
 
 autoload predict-on
+
+#zle -N predict-on
+#zle -N predict-off
+
+alias predict-on="predict-on && export ZSH_PREDICT_STATE=0"
+alias predict-off="predict-off && export ZSH_PREDICT_STATE=1"
+
 predict-on
+
+predict-toggle()
+{
+  if [ $ZSH_PREDICT_STATE -eq 0 ]
+  then predict-off;
+  else predict-on;
+  fi;
+}
+
+zle -N predict-toggle
+
+bindkey "^[[2~" predict-toggle
+
+zstyle ':predict' verbose true
 
 setopt multios
