5427618e1d7235709ec844d6edfb22c45367b874
Usagi Ito <usagi@WonderRabbitProject.net>
2013-12-17 08:04:52 +0900

    add variable PI and degrees/radians function

--- .zshrc.d/calc.zsh
+++ .zshrc.d/calc.zsh
@@ -1,2 +1,7 @@
 zmodload -i zsh/mathfunc
 calc(){ if [ $# != 0 ]; then echo "$(( $* ))"; fi; }
+
+export PI="3.14159265358979323846264338327950288"
+
+degrees(){ if [ $# != 0 ]; then calc "1. * $* * 180 / $PI"; fi; }
+radians(){ if [ $# != 0 ]; then calc "1. * $* * $PI / 180"; fi; }
