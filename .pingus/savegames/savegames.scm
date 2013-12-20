11999bac5766388ee9a4558a7ce25ca54cbc49ad
Usagi Ito <usagi@WonderRabbitProject.net>
2013-12-21 00:29:57 +0900

    add pingus settings

---
+++ .pingus/savegames/savegames.scm
@@ -0,0 +1,111 @@
+(pingus-savegame 
+  (level 
+    (filename "")
+    (status "accessible")
+    (time 6459)
+    (saved-pingus 0))
+  (level 
+    (filename "tutorial/digger-tutorial2-grumbel")
+    (status "finished")
+    (time 1994)
+    (saved-pingus 10))
+  (level 
+    (filename "tutorial/snow11-grumbel")
+    (status "finished")
+    (time 3360)
+    (saved-pingus 8))
+  (level 
+    (filename "tutorial/floater-tutorial-grumbel")
+    (status "finished")
+    (time 3138)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/snow8-grumbel")
+    (status "finished")
+    (time 5490)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/basher-tutorial-grumbel")
+    (status "finished")
+    (time 3371)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/snow9-grumbel")
+    (status "finished")
+    (time 3369)
+    (saved-pingus 15))
+  (level 
+    (filename "tutorial/snow10-grumbel")
+    (status "finished")
+    (time 5473)
+    (saved-pingus 36))
+  (level 
+    (filename "tutorial/jumper-tutorial-grumbel")
+    (status "finished")
+    (time 3286)
+    (saved-pingus 12))
+  (level 
+    (filename "tutorial/miner-tutorial2-grumbel")
+    (status "finished")
+    (time 5415)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/bomber-tutorial2-grumbel")
+    (status "finished")
+    (time 4221)
+    (saved-pingus 24))
+  (level 
+    (filename "tutorial/snow14-grumbel")
+    (status "finished")
+    (time 6848)
+    (saved-pingus 28))
+  (level 
+    (filename "tutorial/solid-tutorial-grumbel")
+    (status "finished")
+    (time 5023)
+    (saved-pingus 16))
+  (level 
+    (filename "tutorial/snow19-grumbel")
+    (status "finished")
+    (time 8920)
+    (saved-pingus 11))
+  (level 
+    (filename "tutorial/snow21-grumbel")
+    (status "finished")
+    (time 2675)
+    (saved-pingus 10))
+  (level 
+    (filename "tutorial/snow15-grumbel")
+    (status "finished")
+    (time 3309)
+    (saved-pingus 28))
+  (level 
+    (filename "tutorial/snow20-grumbel")
+    (status "finished")
+    (time 5299)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/snow17-grumbel")
+    (status "finished")
+    (time 5831)
+    (saved-pingus 12))
+  (level 
+    (filename "tutorial/snow16-grumbel")
+    (status "finished")
+    (time 7941)
+    (saved-pingus 18))
+  (level 
+    (filename "tutorial/snow22-grumbel")
+    (status "finished")
+    (time 11524)
+    (saved-pingus 21))
+  (level 
+    (filename "tutorial/snow7-grumbel")
+    (status "finished")
+    (time 9184)
+    (saved-pingus 20))
+  (level 
+    (filename "tutorial/snow12-grumbel")
+    (status "finished")
+    (time 2769)
+    (saved-pingus 10)))
