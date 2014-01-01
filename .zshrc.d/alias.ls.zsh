a38d9adc849a4520617e4a2e525adaa9840d43a4
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-02 04:45:25 +0900

    fix ls option(show sym-link)

--- .zshrc.d/alias.ls.zsh
+++ .zshrc.d/alias.ls.zsh
@@ -1,2 +1,2 @@
-alias ls="ls -ahHLpvx --group-directories-first --color=auto --time-style=+%Y-%m-%dT%H:%M:%S"
+alias ls="ls -ahHpvx --group-directories-first --color=auto --time-style=+%Y-%m-%dT%H:%M:%S"
 alias ll="ls -l"
