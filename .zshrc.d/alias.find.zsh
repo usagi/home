26b6b189ef4d960e1c197b90c4105b0e36b0bbbf
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-03 18:39:43 +0900

    find aliasの強化修正

--- .zshrc.d/alias.find.zsh
+++ .zshrc.d/alias.find.zsh
@@ -1,9 +1,37 @@
 find.bin()
 {
-  find `echo $PATH | tr : " "` -name $1 2>/dev/null
+  find \
+    `echo $PATH | tr : " "` \
+    ~/opt/bin \
+    /opt/local/bin \
+    -iname "*$1*" \
+    2>/dev/null \
+  | sort | uniq
 }
 
 find.include()
 {
-  find `echo $CPLUS_INCLUDE_PATH | tr : " "` /usr/local/include /usr/include -name $1 2>/dev/null
+  find \
+    `echo $CPLUS_INCLUDE_PATH | tr : " "` \
+    ~/opt/include \
+    /usr/local/include \
+    /usr/include \
+    /opt/local/include \
+    -iname "*$1*" \
+    2>/dev/null \
+  | sort | uniq
 }
+
+find.lib()
+{
+  find \
+    `echo $LD_LIBRARY_PATH | tr : " "` \
+    ~/opt/lib \
+    /usr/local/lib \
+    /usr/lib \
+    /opt/local/lib \
+    -iname "*$1*" \
+    2>/dev/null \
+  | sort | uniq
+}
+
