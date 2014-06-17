e2e0cda2de87303c274a98f153885c274f4f2163
Usagi Ito <usagi@WonderRabbitProject.net>
2014-06-18 07:28:33 +0900

    fix

--- .zshrc.d/mozc.zsh
+++ .zshrc.d/mozc.zsh
@@ -1,7 +1,9 @@
-mozc_server_start
-export XIM=ibus
-export GTK_IM_MODULE=ibus
-export QT_IM_MODULE=xim
-export XMODIFIERS=@im=ibus
-export XIM_PROGRAM="ibus-daemon"
-export XIM_ARGS="-r --daemonize --xim"
+if [ `uname -s` = 'FreeBSD' ]; then
+  mozc_server_start
+  export XIM=ibus
+  export GTK_IM_MODULE=ibus
+  export QT_IM_MODULE=xim
+  export XMODIFIERS=@im=ibus
+  export XIM_PROGRAM="ibus-daemon"
+  export XIM_ARGS="-r --daemonize --xim"
+fi
