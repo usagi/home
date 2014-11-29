f0ce18d735326fdf5f1ecbde0ef6d3049fe7d492
Usagi Ito <usagi@WonderRabbitProject.net>
2014-11-29 14:21:11 +0900

    check environment for alias of debian

--- .zshrc.d/alias.debian.zsh
+++ .zshrc.d/alias.debian.zsh
@@ -1,5 +1,22 @@
 if [ -f /etc/debian_version ]; then
-  alias ack="ack-grep"
-  alias chromium="chromium-browser"
-  alias chrome="google-chrome"
+  echo '[zshrc/alias.debian]'
+  if which ack > /dev/null; then
+    echo '  ack             : found';
+  else
+    echo '  ack             : not found'
+    if which ack-grep > /dev/null; then
+      echo '    ack-grep      : found; set alias to ack'
+      alias ack="ack-grep"
+    else
+      echo '    WARN          : cannot use ack'
+    fi
+  fi
+  if which chromium-browser > /dev/null; then
+    echo '  chromium-browser: found; set alias to chromium'
+    alias chromium="chromium-browser"
+  fi
+  if which google-chrome > /dev/null; then
+    echo '  google-chrome   : found; set alias chrome'
+    alias chrome="google-chrome"
+  fi
 fi
