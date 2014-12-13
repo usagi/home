e1ea8c33b65e50c19f21fe33c69bf6021c8cb492
Usagi Ito <usagi@WonderRabbitProject.net>
2014-12-13 17:31:11 +0900

    add vim.tab and vim.tab.c++

---
+++ .zshrc.d/vim.zsh
@@ -0,0 +1,2 @@
+alias vim.tab='vim -p'
+alias vim.tab.c++='vim.tab $(find . -iname "*.cxx" -or -iname "*.cpp" -or -iname "*.c++" -or -iname "*.c" -or -iname "*.hxx" -or -iname "*.hpp" -or -iname "*.h++" -or -iname "*.h")'
